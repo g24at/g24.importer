250d7458134178a489cc20ae31e00ac614f33321
Johannes Raggam <raggam-nl@adm.at>
2012-06-07 17:01:57 +0200

    set layout for streamfolder to stream. whitespace remove, import sort

--- src/g24/importer/setup_xml_import.py
+++ src/g24/importer/setup_xml_import.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
-
-import time
-from xml.dom.minidom import parse
-
-from Products.CMFPlone.utils import normalizeString
-from Products.CMFCore.utils import getToolByName
-
-from Products.CMFPlone.utils import _createObjectByType
-from Products.Archetypes.event import ObjectInitializedEvent
-from zope import event
 import collective.setuphandlertools as sht
-
 from DateTime import DateTime
-
-from g24.elements.sharingbox.form import create, add, edit
+from xml.dom.minidom import parse
 from g24.elements.sharingbox.form import FEATURES, IGNORES, G24_BASETYPE
+from g24.elements.sharingbox.form import create, add, edit
 
 logger = logging.getLogger("g24.importer from xml")
 
@@ -23,10 +12,10 @@ class ImportPhpBBPostings(object):
 
     def __init__(self, context):
         self.context = context
-        
+
         importpath = "src/g24.importer/src/g24/importer/scripts/export-posts.xml"
 
-        self.content_dom = False        
+        self.content_dom = False
         try:
             self.content_dom = parse(importpath)
         except Exception as err:
@@ -36,7 +25,7 @@ class ImportPhpBBPostings(object):
         self.conn = None
         self.pt = getToolByName(context, 'portal_transforms')
         """
-        
+
         #wft = getToolByName(site, 'portal_workflow')
         #wft.doActionFor(site['forum'], 'publish')
 
@@ -45,41 +34,41 @@ class ImportPhpBBPostings(object):
         if not self.content_dom:
             logger.error("no xml import found")
             return
-        
+
         for th in self.content_dom.getElementsByTagName('thread'):
             try:
                 logger.info("import thread")
-                
+
                 postings = th.getElementsByTagName('post')
-    
+
                 if (len(postings)<1):
                     logger.warning("thread without postings")
                     continue
-                
+
                 mainpost = self.create_g24_posting(self.context, self.map_xml_to_post(postings[0]))
-                
+
                 for p in postings[1:len(postings)]:
                     self.create_g24_posting(mainpost, self.map_xml_to_post(p))
             except Exception as err:
                 logger.error('Failed to import thread ... ')
                 #logger.error(err.)
-    
+
     def map_xml_to_post(self, node):
         data = {}
-        
+
         data['title']   = node.getAttribute('post_subject')
         data['text']    = node.getElementsByTagName('text')[0].childNodes[1].data
-        
+
         data['tags']    = []
         for t in node.getElementsByTagName('tag'):
             data['tags'].append(t.getAttribute('name'))
-        
+
         data['username']    = node.getAttribute('username')
         #data['tstamp']      = node.getAttribute('post_time')
         data['post_time']      = node.getAttribute('post_time')
-        
+
         return data
-        
+
     def create_g24_posting(self, container, postingdata):
         data = {
             'is_title': True,
@@ -91,7 +80,7 @@ class ImportPhpBBPostings(object):
         obj = create(container, G24_BASETYPE)
         obj = add(obj, container)
         obj.setCreators(postingdata['username']) # set the creators by loginname. if more than one, seperate by whitespace
-        
+
         obj.creation_date = DateTime(postingdata['post_time'])
 
         edit(obj, data, order=FEATURES, ignores=IGNORES)
@@ -114,8 +103,9 @@ def start_import(context):
     streamfolder = create(site, G24_BASETYPE)
     streamfolder.id = 'stream'
     streamfolder = add(streamfolder, site)
+    streamfolder.setLayout('stream')
     edit(streamfolder, data, order=FEATURES, ignores=IGNORES)
-    
+
     # start import into stream folder
     imp = ImportPhpBBPostings(site.stream)
     imp.import_content()
