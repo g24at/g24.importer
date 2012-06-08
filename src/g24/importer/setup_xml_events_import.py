883812630af60a995338151ba6a659f92660db12
mf <mf@tt4.at>
2012-06-08 15:35:25 +0200

    fix unicode err, added end-date calcualtion

--- src/g24/importer/setup_xml_events_import.py
+++ src/g24/importer/setup_xml_events_import.py
@@ -17,56 +17,49 @@ from DateTime import DateTime
 from g24.elements.sharingbox.form import create, add, edit
 from g24.elements.sharingbox.form import FEATURES, IGNORES, G24_BASETYPE
 
+from yafowil.base import UNSET
+from plone.event.utils import pydt
+
 logger = logging.getLogger("g24.importer from xml")
 
 class ImportEvents(object):
 
     def __init__(self, context):
         self.context = context
-        
         importpath = "src/g24.importer/src/g24/importer/scripts/export-events.xml"
-
         self.content_dom = False        
         try:
             self.content_dom = parse(importpath)
         except Exception as err:
             logger.error("could not open " + importpath + ", maybe you have to run scripts/export-events.py to create it")
 
-
     def import_content(self):
         if not self.content_dom:
             logger.error("no xml import found")
             return
         
+        ok = 0
+        fail = 0
         for ev in self.content_dom.getElementsByTagName('event'):
-            self.create_g24_event(self.context, ev)
-            
-            """
             try:
-                logger.info("import event...")
-                self.create_g24_posting(self.context, ev)
-                
+                self.create_g24_event(self.context, ev)
+                ok += 1
             except Exception as err:
-                logger.error('Failed to import posting ... ')
-            """
+                logger.error('Failed to import event ( id ' + ev.getAttribute('pc_eid') + ')... ' + repr(err))
+                fail +=1
     
+        logger.info('Import done , ok: ' + str(ok) + ' , failed : ' + str(fail))
+        
     def create_g24_event(self, container, node):
 
-        data = {}
-        
         title = node.getAttribute('pc_title')
         text  = node.getElementsByTagName('text')[0].childNodes[1].data
-        
         tags = []
         for t in node.getElementsByTagName('tag'):
             tags.append(t.getAttribute('name'))
         
-        # data['username']    = node.getAttribute('username')
-        # data['tstamp']      = node.getAttribute('post_time')
-        
-        from yafowil.base import UNSET
         data = {
-            'is_title': str(title).strip() != "",
+            'is_title': title.strip() != "",
             'is_event': True,
             'title': title,
             'text': text,
@@ -74,22 +67,22 @@ class ImportEvents(object):
             'timezone':'Europe/Vienna',
             'whole_day': node.getAttribute('pc_alldayevent') == "1",
             'recurrence':  UNSET,
-            'location': node.getAttribute('pc_time'),
+            'location': node.getAttribute('location_name'),
         }
-
-        from plone.event.utils import pydt
+        
         createdate      = DateTime(node.getAttribute('pc_time'))
         event_date      = DateTime(node.getAttribute('pc_eventDate') + " " + node.getAttribute('pc_startTime') + " CET")
-        # import pdb; pdb.set_trace()
         data['start']   = pydt(event_date)
         
-        # todo calc / use end date -- use "duration" if set
+        # calc / use end date , fallback is same as start
         data['end']     = pydt(event_date)
-        if node.getAttribute('pc_endDate') != "None":
-            end_date      = DateTime(node.getAttribute('pc_endDate') + " " + node.getAttribute('pc_endTime') + " CET")
-            data['end']     = pydt(end_date)
-         
-        
+        if node.hasAttribute('pc_endDate'):
+            end_date = DateTime(node.getAttribute('pc_endDate') + " " + node.getAttribute('pc_endTime') + " CET")
+            data['end']   = pydt(end_date)
+        elif node.getAttribute('pc_duration'):
+            end_date = DateTime(int(event_date) + int(node.getAttribute('pc_duration')))
+            data['end']   = pydt(end_date)
+                
         obj = create(container, G24_BASETYPE)
         obj = add(obj, container)
         
