e909e3a2cfb5b653d2849006142053088909a821
Johannes Raggam <raggam-nl@adm.at>
2013-10-25 17:16:11 +0200

    pep8

--- src/g24/importer/scripts/export-events.py
+++ src/g24/importer/scripts/export-events.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from ConfigParser import ConfigParser
 from pnexport import cleanTextFromControlChars
 from xml.dom.minidom import Document
@@ -23,40 +22,39 @@ conn = MySQLdb.connect(host=cfg.get('default', 'mysql.host'),
                        db=cfg.get('default', 'mysql.db'))
 
 """
-
 mysql> describe nuke_postcalendar_events;
-+----------------+-----------------------+------+-----+------------+----------------+
-| Field          | Type                  | Null | Key | Default    | Extra          |
-+----------------+-----------------------+------+-----+------------+----------------+
-| pc_eid         | int(11) unsigned      | NO   | PRI | NULL       | auto_increment |
-| pc_aid         | varchar(30)           | NO   |     |            |                |
-| pc_title       | varchar(150)          | YES  |     | NULL       |                |
-| pc_time        | datetime              | YES  |     | NULL       |                |
-| pc_hometext    | text                  | YES  |     | NULL       |                |
-| pc_comments    | int(11)               | YES  |     | 0          |                |
-| pc_counter     | mediumint(8) unsigned | YES  |     | NULL       |                |
-| pc_topic       | int(3)                | NO   |     | 1          |                |
-| pc_informant   | varchar(20)           | NO   |     |            |                |
-| pc_eventDate   | date                  | NO   |     | 0000-00-00 |                |
-| pc_endDate     | date                  | NO   |     | 0000-00-00 |                |
-| pc_recurrtype  | int(1)                | NO   |     | 0          |                |
-| pc_recurrspec  | text                  | YES  |     | NULL       |                |
-| pc_recurrfreq  | int(3)                | NO   |     | 0          |                |
-| pc_startTime   | time                  | YES  |     | NULL       |                |
-| pc_endTime     | time                  | YES  |     | NULL       |                |
-| pc_alldayevent | int(1)                | NO   |     | 0          |                |
-| pc_location    | text                  | YES  |     | NULL       |                |
-| pc_conttel     | varchar(50)           | YES  |     | NULL       |                |
-| pc_contname    | varchar(150)          | YES  |     | NULL       |                |
-| pc_contemail   | varchar(255)          | YES  |     | NULL       |                |
-| pc_website     | varchar(255)          | YES  |     | NULL       |                |
-| pc_fee         | varchar(50)           | YES  |     | NULL       |                |
-| pc_eventstatus | int(11)               | NO   |     | 0          |                |
-| pc_catid       | int(11)               | NO   | MUL | 0          |                |
-| pc_duration    | bigint(20)            | NO   |     | 0          |                |
-| pc_sharing     | int(11)               | NO   |     | 0          |                |
-| pc_language    | varchar(30)           | YES  |     |            |                |
-+----------------+-----------------------+------+-----+------------+----------------+
++----------------+-----------------------+------+-----+------------+----------+
+| Field          | Type                  | Null | Key | Default    | Extra    |
++----------------+-----------------------+------+-----+------------+----------+
+| pc_eid         | int(11) unsigned      | NO   | PRI | NULL       | auto_inc |
+| pc_aid         | varchar(30)           | NO   |     |            |          |
+| pc_title       | varchar(150)          | YES  |     | NULL       |          |
+| pc_time        | datetime              | YES  |     | NULL       |          |
+| pc_hometext    | text                  | YES  |     | NULL       |          |
+| pc_comments    | int(11)               | YES  |     | 0          |          |
+| pc_counter     | mediumint(8) unsigned | YES  |     | NULL       |          |
+| pc_topic       | int(3)                | NO   |     | 1          |          |
+| pc_informant   | varchar(20)           | NO   |     |            |          |
+| pc_eventDate   | date                  | NO   |     | 0000-00-00 |          |
+| pc_endDate     | date                  | NO   |     | 0000-00-00 |          |
+| pc_recurrtype  | int(1)                | NO   |     | 0          |          |
+| pc_recurrspec  | text                  | YES  |     | NULL       |          |
+| pc_recurrfreq  | int(3)                | NO   |     | 0          |          |
+| pc_startTime   | time                  | YES  |     | NULL       |          |
+| pc_endTime     | time                  | YES  |     | NULL       |          |
+| pc_alldayevent | int(1)                | NO   |     | 0          |          |
+| pc_location    | text                  | YES  |     | NULL       |          |
+| pc_conttel     | varchar(50)           | YES  |     | NULL       |          |
+| pc_contname    | varchar(150)          | YES  |     | NULL       |          |
+| pc_contemail   | varchar(255)          | YES  |     | NULL       |          |
+| pc_website     | varchar(255)          | YES  |     | NULL       |          |
+| pc_fee         | varchar(50)           | YES  |     | NULL       |          |
+| pc_eventstatus | int(11)               | NO   |     | 0          |          |
+| pc_catid       | int(11)               | NO   | MUL | 0          |          |
+| pc_duration    | bigint(20)            | NO   |     | 0          |          |
+| pc_sharing     | int(11)               | NO   |     | 0          |          |
+| pc_language    | varchar(30)           | YES  |     |            |          |
++----------------+-----------------------+------+-----+------------+----------+
 
 """
 
@@ -76,7 +74,8 @@ def safe_unicode(value, encoding='utf-8'):
 
 
 def _cleanup_textonly(s):
-    # http://stackoverflow.com/questions/730299/replace-html-entities-with-the-corresponding-utf-8-characters-in-python-2-6
+    # http://stackoverflow.com/questions/730299/replace-html-entities-with-the
+    #corresponding-utf-8-characters-in-python-2-6
     s = s.strip()
     pars = HTMLParser.HTMLParser()
     result = pars.unescape(s)
@@ -86,7 +85,8 @@ def _cleanup_textonly(s):
     #    '&lt;': '<',
     #    '&gt;': '>',
     #}
-    ## http://stackoverflow.com/questions/2400504/easiest-way-to-replace-a-string-using-a-dictionary-of-replacements/2401481#2401481
+    ## http://stackoverflow.com/questions/2400504/easiest-way-to-replace-a-str
+    #ing-using-a-dictionary-of-replacements/2401481#2401481
     ## no whole words
     #pattern = re.compile('|'.join(d.keys()))
     ## whole words
@@ -94,37 +94,43 @@ def _cleanup_textonly(s):
     #result = pattern.sub(lambda x: d[x.group()], s)
     return safe_unicode(result)
 
-
 dom = Document()
 dom.appendChild(dom.createElement('export'))
 
 # load categories lookup
 sql_str = "select * from nuke_postcalendar_categories"
-cursor = conn.cursor (MySQLdb.cursors.DictCursor)
-cursor.execute (sql_str);
+cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+cursor.execute(sql_str)
 rows = cursor.fetchall()
 categories = {}
 for cat in rows:
-    categories[str(cat['pc_catid'])] = cleanTextFromControlChars(str(cat['pc_catname']))
+    categories[str(cat['pc_catid'])] =\
+        cleanTextFromControlChars(str(cat['pc_catname']))
 
 test_set = True
 custom_limit = ""
-if len (sys.argv) > 1:
-    if sys.argv[1] == "all" : test_set = False
-    else : test_set = False ; custom_limit = sys.argv[1]
+if len(sys.argv) > 1:
+    if sys.argv[1] == "all":
+        test_set = False
+    else:
+        test_set = False
+        custom_limit = sys.argv[1]
 
 # select topics for export
 sql_str = "select * from nuke_postcalendar_events where pc_eventstatus = 1"
-if      test_set            : sql_str = sql_str + " order by RAND() limit 0,200"
-elif    custom_limit != ""  : sql_str = sql_str + " " + custom_limit
+if test_set:
+    sql_str = sql_str + " order by RAND() limit 0,200"
+elif custom_limit != "":
+    sql_str = sql_str + " " + custom_limit
 
 #sql_str = sql_str + " and pc_eid < 3000 and pc_eid > 2800"
 #sql_str = sql_str + " and pc_eid = 47 "
-#sql_str = sql_str + " and pc_eid in (" + ','.join([str(2544),str(2541)]) + ") "
+#sql_str = sql_str + " and pc_eid in (" + ','.join([str(2544),str(2541)]) +
+#                                     ") "
 #sql_str = sql_str + " order by RAND() limit 0,20"
 
-eventcursor = conn.cursor (MySQLdb.cursors.DictCursor)
-eventcursor.execute (sql_str);
+eventcursor = conn.cursor(MySQLdb.cursors.DictCursor)
+eventcursor.execute(sql_str)
 eventrows = eventcursor.fetchall()
 
 items_ok = 0
@@ -483,47 +489,48 @@ for event in eventrows:
 
     # export fields
     fields = [
-                'pc_contname',
-                'pc_eid',
-                'pc_fee',
-                'pc_contemail',
-                'pc_duration',
-                'pc_website',
-               # 'pc_aid',
-               # 'pc_recurrfreq',
-                'pc_eventstatus',
-                'pc_time',
-                'pc_informant',
-
-                'pc_conttel',
-                'pc_alldayevent',
-              # 'pc_recurrspec',
-                'pc_eventDate',
-              #  'pc_recurrtype',
-                'pc_startTime',
-                'pc_topic'
-                ]
+        'pc_contname',
+        'pc_eid',
+        'pc_fee',
+        'pc_contemail',
+        'pc_duration',
+        'pc_website',
+        # 'pc_aid',
+        # 'pc_recurrfreq',
+        'pc_eventstatus',
+        'pc_time',
+        'pc_informant',
+        'pc_conttel',
+        'pc_alldayevent',
+        # 'pc_recurrspec',
+        'pc_eventDate',
+        #  'pc_recurrtype',
+        'pc_startTime',
+        'pc_topic'
+    ]
     for attr in fields:
         eventnode.setAttribute(attr, str(event[attr]).decode(IN_ENCODING))
 
     # only set end date if value is set
     for attr in ['pc_endDate', 'pc_endTime']:
         if event[attr]:
-            eventnode.setAttribute(attr , str(event[attr]).decode(IN_ENCODING))
+            eventnode.setAttribute(attr, str(event[attr]).decode(IN_ENCODING))
 
-    title = cleanTextFromControlChars(str(event["pc_title"]).decode(IN_ENCODING))
-    eventnode.setAttribute("pc_title" , title)
+    title = cleanTextFromControlChars(
+        str(event["pc_title"]).decode(IN_ENCODING)
+    )
+    eventnode.setAttribute("pc_title", title)
 
     # clean + add event text
     posting_text = str(event['pc_hometext']).decode(IN_ENCODING)
     posting_text = cleanTextFromControlChars(posting_text)
     lines = posting_text.split("\n")
     if ':text:' in lines[0]:
-        lines[0] = re.sub(':text:','', lines[0])
-        posting_text= "<br/>".join(lines)
+        lines[0] = re.sub(':text:', '', lines[0])
+        posting_text = "<br/>".join(lines)
     elif ':html:' in lines[0]:
-        lines[0] = re.sub(':html:','', lines[0])
-        posting_text= "".join(lines)
+        lines[0] = re.sub(':html:', '', lines[0])
+        posting_text = "".join(lines)
 
     txt = dom.createElement('text')
     txt.appendChild(dom.createCDATASection(posting_text))
@@ -554,7 +561,7 @@ print "events export done! ok:", items_ok, ", failed:", items_fail
 ### LOCATIONS
 
 # case insensitive sorting
-locations = collections.OrderedDict(sorted(locations.items()))  #, key=str.lower))
+locations = collections.OrderedDict(sorted(locations.items()))
 
 
 dom = Document()
